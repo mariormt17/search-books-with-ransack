0f933d73f1348f7bb22f4c452e3120db81456cf3
Mario Mancilla <mariormt17@gmail.com>
2021-09-23 14:49:53 -0500

    Add some books on seeds file to generate them

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,11 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+100.times do
+  Book.create(
+    isbn: Faker::Code.isbn,
+    name: Faker::Book.title,
+    author: Faker::Book.author,
+    year: Faker::Number.between(1900, 2019),
+    genre: Faker::Number.between(1, 5),
+    price: Faker::Commerce.price,
+    status: Faker::Boolean.boolean
+  )
+end
